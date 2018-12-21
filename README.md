ce99737729c21f71ecc379c6420d88a6ea8fbbb2
Luis Michael <michael.ibarra@gmail.com>
2018-12-20 18:17:08 -0700

    using pkg/errors

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 - [Go tour](https://tour.golang.org/)
 - [Interfaces](https://medium.com/golangspec/interfaces-in-go-part-i-4ae53a97479c)
-- [Errors](https://medium.com/@sebdah/go-best-practices-error-handling-2d15e1f0c5ee)
+
+##### Errors
+- [Error 1](https://medium.com/@sebdah/go-best-practices-error-handling-2d15e1f0c5ee)
+- [Error 2](https://medium.com/@hussachai/error-handling-in-go-a-quick-opinionated-guide-9199dd7c7f76)
+- [Error 3](https://hackernoon.com/golang-handling-errors-gracefully-8e27f1db729f)
--- concepts/errors.go
+++ concepts/errors.go
@@ -3,7 +3,8 @@ package concepts
 import (
 	"fmt"
 	"strconv"
-	"errors"
+//	"errors"
+	"github.com/pkg/errors"		//Using this package implements also the errors package
 )
 
 //It creates a new struct for errors.
@@ -12,6 +13,15 @@ type ErrInvalid struct {
 }
 
 // ErrInvalid implements the Error interface.
+// Error is an interface type.
+// The standard error doesn't come with stack traces. Check pkg/errors.
+/**
+
+type error interface {
+	Error() string
+}
+**/
+
 func (e *ErrInvalid) Error() string {
 	return e.message
 }
@@ -28,9 +38,9 @@ func NewErrInvalid(message string) *ErrInvalid {
 var ErrInvalidRange = errors.New("Invalid Rage") //Defining an error with the errors package.
 
 func Convert(number string) {
-	n, err := strconv.Atoi(number)
+	n, err := strconv.Atoi(number)	// value in the right, error in the left by convention.
 	if err != nil {
-		fmt.Println("not a valid number")
+		fmt.Println(errors.Wrapf(err, "not a valid number"))
 	} else if n <= 10 {
 		fmt.Println(NewErrInvalid("Number too small"))
 		//fmt.Println(&ErrInvalid{"Number too small"})	//This implements the interface without the need of NewErrInvalid() 
